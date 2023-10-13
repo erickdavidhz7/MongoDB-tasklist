97844ce3cd8a42103021c3f59259c77c0ef19a6f
Erick Henriquez <erickdavid7@hotmail.com>
2023-10-13 16:28:27 -0500

    eliminado creación de campo _v

--- schemas/taskModel.js
+++ schemas/taskModel.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-const taskSchema = mongoose.Schema({
-  name: { type: String, required: [true, "name is required"] },
-  description: { type: String, required: [true, "description is required"] },
-  status: {
-    type: String,
-    required: [true, "status is required"],
-    enum: ["Completed", "Not completed"],
+const taskSchema = mongoose.Schema(
+  {
+    name: { type: String, required: [true, "name is required"] },
+    description: { type: String, required: [true, "description is required"] },
+    status: {
+      type: String,
+      required: [true, "status is required"],
+      enum: ["Completed", "Not completed"],
+    },
   },
-});
+  { versionKey: false }
+);
 
 const TaskModel = mongoose.model("tasks", taskSchema);
 
--- schemas/userModel.js
+++ schemas/userModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  email: { type: String, required: [true, "email is required"] },
-  password: { type: String, required: [true, "password is required"] },
-  role: { type: String, required: false},
-});
+const userSchema = mongoose.Schema(
+  {
+    email: { type: String, required: [true, "email is required"] },
+    password: { type: String, required: [true, "password is required"] },
+    role: { type: String, required: false },
+  },
+  { versionKey: false }
+);
 
 const UserModel = mongoose.model("users", userSchema);
 
